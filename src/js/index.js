19a4f87d7557ce1ba19e27732f8658571759b38c
Adrian <adrian.zawadzki@outlook.com>
2018-11-22 19:31:07 +0100

    building search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,48 @@
-const a = 1;
+import Search from './models/Search';
+
+/** Global state of the app
+ * - Search object (I have here search query and search result)
+ * - Current artist object
+ * - Albums object
+ */
+
+const state = {};
+
+const headerSearch = async () => {
+    // 1. Get query from view
+    const query = 'pl';
+
+    if (query) {
+        // 2. New artist object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for result
+
+        // 4. Search for artist
+        await state.search.getResults(); // this will return promise (all async function return promise)
+
+        // 5. Render result on UI
+        console.log(state.search.result);
+    }
+};
+
+
+
+
+
+document.querySelector('.header__search').addEventListener('submit', e => {
+    e.preventDefault();
+    headerSearch();
+
+});
+
+
+
+
+
+
+
+
+// overwrite(countries)
+// const { getCode } = require('country-list');
+// console.log(getCode('Poland'))
